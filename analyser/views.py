da20acdafebb0ec8dd6c5d4fde856efdeb23fb8a
Jules LE BRIS <jls.lebris@gmail.com>
2019-11-26 09:42:39 +0100

    :beers: work with image

--- analyser/views.py
+++ analyser/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
 
 # Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
 def index(request):
+    if request.method == 'POST':
+        print(request.FILES)
+        print(pytesseract.image_to_string(Image.open(request.FILES[0])))
     return HttpResponse("Hello, world. You're at the polls index.")
